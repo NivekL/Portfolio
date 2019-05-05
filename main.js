6f3888cf814b47bf22ac119fa664d947be4fbc41
Kevin Leskinen <leskinen43@gmail.com>
2019-05-05 17:33:04 +0200

    creating portfolio

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function (){
+  var allTiles = $('.tile-content').hide();
+  $('.tile-wrap').click(function() {
+      $this = $(this);
+      $target =  $this.next();
+      if($target.hasClass('active')){
+        $('.tile-title').removeClass("closed");
+        $target.removeClass('active').slideUp();
+      }else{
+        $('.tile-title').removeClass("closed");
+        $target.prev().children().addClass("closed");
+        allTiles.removeClass('active').slideUp();
+        $target.addClass('active').slideDown();
+      }
+    return false;
+  });
+
+  /*Skillbar function*/
+  $('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},6000);
+	});
+});
